4b8d5295763d8c56eebdd6c3a66cdf800af74086
Kittaemetsanglaw <143715259+Kittaemetsanglaw@users.noreply.github.com>
2025-04-11 20:03:50 +0700

    V1 addpage
    add Dessert Drinks Salad in StackNavigator

--- component/StackNavigator.js
+++ component/StackNavigator.js
@@ -8,6 +8,10 @@ import MealPage from './Category/MealPage';
 import AppetizersPage from './/Category/AppetizersPage';
 import MealDetailPage from './Category/Detail/MealDetailPage';
 import AppetizersDetailPage from './Category/Detail/AppetizersDetailPage';
+import DessertPage from './Category/DessertPage';
+import DessertDetailPage from './Category/Detail/DessertDetailPage';
+import SaladPage from './Category/SaladPage';
+import SaladDetailPage from './Category/Detail/SaladDetailPage';
 
 const Stack = createStackNavigator();
 
@@ -29,13 +33,29 @@ export default function StackNavigator() {
           options={{ title: 'ชื่อเมนู', headerShown: false }} />
         <Stack.Screen 
           name="AppetizersPage" 
-          component={AppetizersPage} 
+          component={AppetizersPage}  
           options={{ title: 'Appetizers', headerShown: false }} />
         <Stack.Screen 
           name="AppetizersDetail"
           component={AppetizersDetailPage}
           options={{ title: 'ชื่อเมนู', headerShown: false }} />
-      </Stack.Navigator>
+        <Stack.Screen 
+          name="DessertPage" 
+          component={DessertPage} 
+          options={{ title: 'Dessert', headerShown: false }} />
+        <Stack.Screen 
+          name="DessertDetail"
+          component={DessertDetailPage}
+          options={{ title: 'ชื่อเมนู', headerShown: false }} />
+          <Stack.Screen 
+          name="SaladPage" 
+          component={SaladPage} 
+          options={{ title: 'Dessert', headerShown: false }} />
+        <Stack.Screen 
+          name="SaladDetail"
+          component={SaladDetailPage}
+          options={{ title: 'ชื่อเมนู', headerShown: false }} />
+      </Stack.Navigator>  
     </NavigationContainer>
   );
 }
